3685d7e12d203837bd1a3fed8a013160213d27eb
nataliaworm <nataliaworm@gmail.com>
2024-05-02 00:35:32 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,49 @@
+function scrollToSectionBio() {
+  // Rola a página até a seção específica
+  var dados = document.getElementById('secaoDadosBio');
+  
+  if (dados) {
+    secaoDadosBio.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+function scrollToSectionAgenda() {
+  // Rola a página até a seção específica
+  var dados = document.getElementById('secaoDadosAgenda');
+  
+  if (dados) {
+    secaoDadosAgenda.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+function scrollToSectionPhoto() {
+  var dados = document.getElementById('secaoDadosPhoto');
+  
+  if (dados) {
+    secaoDadosPhoto.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+function scrollToSectionVideo() {
+  var dados = document.getElementById('secaoDadosVideo');
+  
+  if (dados) {
+    secaoDadosVideo.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+function scrollToSectionDiscography() {
+  var dados = document.getElementById('secaoDadosDiscography');
+  
+  if (dados) {
+    secaoDadosDiscography.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+function scrollToSectionContact() {
+  var dados = document.getElementById('secaoDadosContact');
+  
+  if (dados) {
+    secaoDadosContact.scrollIntoView({ behavior: 'smooth' });
+  }
+}
